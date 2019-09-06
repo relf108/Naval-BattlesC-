6befa183fbdcd4bb9babc5d48607744239e459e2
JyeStillitano <jyestillitano@hotmail.com>
2019-09-06 11:35:23 +1000

    jye

---
+++ battleships/src/GameLogic.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using SwinGameSDK;
+
+public class GameLogic
+{
+	public static void Main()
+	{
+        //Opens a new Graphics Window
+        SwinGame.OpenGraphicsWindow("Battle Ships", 800, 600);
+
+        //Load Resources
+        LoadResources();
+
+        SwinGame.PlayMusic(GameMusic("Background"));
+
+        //Game Loop
+        while (SwinGame.WindowCloseRequested() != true && CurrentState != GameState.Quitting)
+        {
+            HandleUserInput();
+            DrawScreen();
+        }
+
+        SwinGame.StopMusic();
+
+        //Free Resources and Close Audio, to end the program.
+        FreeResources();
+
+    }
+}
