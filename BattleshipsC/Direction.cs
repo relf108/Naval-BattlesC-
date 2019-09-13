0273d64ba7ca9fb7f592871af0b6eb45e43f884f
relf108 <tristan.sutton@gmail.com>
2019-09-13 11:29:19 +1000

    Tristan's conversions

---
+++ BattleshipsC/Direction.cs
@@ -0,0 +1,16 @@
+namespace BattleshipsC
+{
+
+    public enum Direction
+    {
+        /// <summary>
+        ///     ''' The ship is oriented left/right
+        ///     ''' </summary>
+        LeftRight,
+
+        /// <summary>
+        ///     ''' The ship is oriented up/down
+        ///     ''' </summary>
+        UpDown
+    }
+}
---
+++ BattleshipsC/ShipName.cs
@@ -0,0 +1,12 @@
+namespace BattleshipsC
+{
+    public enum ShipName
+    {
+        None = 0,
+        Tug = 1,
+        Submarine = 2,
+        Destroyer = 3,
+        Battleship = 4,
+        AircraftCarrier = 5
+    }
+}
